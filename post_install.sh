766015c84edbceea0302c120c4370ce436df1b97
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-20 22:30:13 +0200

    Fix some install bugs

--- post_install.sh
+++ post_install.sh
@@ -4,11 +4,11 @@
 FILE_NAME=$(curl -s https://api.github.com/repos/lidarr/Lidarr/releases | jq -r '[[.[] | select(.draft != true) | select(.prerelease == true)][0] | .assets | .[] | select(.name | endswith(".linux.tar.gz")) | .name][0]')
 DOWNLOAD=$(curl -s https://api.github.com/repos/lidarr/Lidarr/releases | jq -r '[[.[] | select(.draft != true) | select(.prerelease == true)][0] | .assets | .[] | select(.name | endswith(".linux.tar.gz")) | .browser_download_url][0]')
   
-iocage exec "${1}" fetch -o /usr/local/share "${DOWNLOAD}"
-"tar -xzvf /usr/local/share/${FILE_NAME} -C /usr/local/share"
+fetch -o /usr/local/share "${DOWNLOAD}"
+tar -xzvf /usr/local/share/${FILE_NAME} -C /usr/local/share
 rm /usr/local/share/"${FILE_NAME}"
 
-"pw user add lidarr -c lidarr -u 353 -d /nonexistent -s /usr/bin/nologin"
+pw user add lidarr -c lidarr -u 353 -d /nonexistent -s /usr/bin/nologin
 chown -R lidarr:lidarr /usr/local/share/Lidarr /config
 
 chmod u+x /usr/local/etc/rc.d/lidarr
